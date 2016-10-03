d15b244dae71eabf0ce9a6a9fdb358fd217e9839
Violeta Sebe <vsebe@ca.ibm.com>
2016-10-03 14:07:24 -0400

    Fix git url for personal forks
    task 117825
    
    Signed-off-by: Violeta Sebe <vsebe@ca.ibm.com>

--- openj9/get_j9_source.sh
+++ openj9/get_j9_source.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
 
 usage() {
-	echo "Usage: $0 [-h|--help] [-j9vm-repo=<user>/j9vm] [-j9vm-branch=<branch>] [-j9jcl-repo=<user>/j9jcl] [-j9jcl-branch=<branch>] [... other OpenJ9 repositories and branches options]"
+	echo "Usage: $0 [-h|--help] [-j9vm-repo=<j9vm repo url>] [-j9vm-branch=<branch>] [-j9jcl-repo=<j9jcl repo url>] [-j9jcl-branch=<branch>] [... other OpenJ9 repositories and branches options]"
 	echo "where:"
 	echo "	-h|--help 			print this help, then exit"
-	echo "	-j9vm-repo			the OpenJ9/vm git fork: default: j9/j9vm "
-	echo "	-j9vm-branch		the OpenJ9/vm git branch: default: master "
-	echo "	-j9jcl-repo			the OpenJ9/j9jcl git fork: default: j9/j9jcl "
-	echo "	-j9jcl-branch		the OpenJ9/j9jcl git branch: default: master "
-	echo "	-omr-repo			the OpenJ9/omr git fork: default: omr/omr "
-	echo "	-omr-branch			the OpenJ9/omr git branch: default: java-master "
-	echo "	-binaries-repo		the OpenJ9/binaries git fork: default: j9/binaries "
-	echo "	-binaries-branch	the OpenJ9/binaries git branch: default: master "
-	echo "	-tooling-repo		the OpenJ9/tooling git fork: default: j9/tooling "
-	echo "	-tooling-branch		the OpenJ9/tooling git branch: default: master "
-	echo "	-rtctest-repo		the OpenJ9/rtctest git fork: default: j9/rtctest "
-	echo "	-test-branch		the OpenJ9/test git branch: default: master "
-	echo "	-test-repo			the OpenJ9/test git fork: default: j9/test "
-	echo "	-rtctest-branch		the OpenJ9/rtctest git branch: default: master "
-	echo "	-jit-repo			the OpenJ9/jit git fork: default: jit/tr.open "
-	echo "	-jit-branch			the OpenJ9/jit git branch: default: java-master "
+	echo "	-j9vm-repo			the OpenJ9/vm repository url: git002@gitlab-polyglot.hursley.ibm.com:j9/j9vm.git"
+	echo "						or <user>@gitlab-polyglot.hursley.ibm.com:<namespace>/j9vm.git"
+	echo "	-j9vm-branch		the OpenJ9/vm git branch: master "
+	echo "	-j9jcl-repo			the OpenJ9/j9jcl repository url: git002@gitlab-polyglot.hursley.ibm.com:joe_dekoning-ca/j9jcl.git"
+	echo "						or <user>@gitlab-polyglot.hursley.ibm.com:<namespace>/j9jcl.git"
+	echo "	-j9jcl-branch		the OpenJ9/j9jcl git branch: master "
+	echo "	-omr-repo			the OpenJ9/omr repository url: git002@gitlab-polyglot.hursley.ibm.com:omr/omr.git"
+	echo "						or <user>@gitlab-polyglot.hursley.ibm.com:<namespace>/omr.git"
+	echo "	-omr-branch			the OpenJ9/omr git branch: java-master "
+	echo "	-binaries-repo		the OpenJ9/binaries repository url: git002@gitlab-polyglot.hursley.ibm.com:j9/binaries.git"
+	echo "						or <user>@gitlab-polyglot.hursley.ibm.com:<namespace>/binaries.git"
+	echo "	-binaries-branch	the OpenJ9/binaries git branch: master "
+	echo "	-tooling-repo		the OpenJ9/tooling repository url: git002@gitlab-polyglot.hursley.ibm.com:j9/tooling.git"
+	echo "						or <user>@gitlab-polyglot.hursley.ibm.com:<namespace>/tooling.git"
+	echo "	-tooling-branch		the OpenJ9/tooling git branch: master "
+	echo "	-rtctest-repo		the OpenJ9/rtctest repository url: git002@gitlab-polyglot.hursley.ibm.com:j9/rtctest.git"
+	echo "						or <user>@gitlab-polyglot.hursley.ibm.com:<namespace>/rtctest.git"
+	echo "	-rtctest-branch		the OpenJ9/test git branch: master "
+	echo "	-test-repo			the OpenJ9/test repository url: git002@gitlab-polyglot.hursley.ibm.com:j9/test.git"
+	echo "						or <user>@gitlab-polyglot.hursley.ibm.com:<namespace>/test.git"
+	echo "	-test-branch		the OpenJ9/test git branch: master "
+	echo "	-jit-repo			the OpenJ9/jit repository url: git002@gitlab-polyglot.hursley.ibm.com:jit/tr.open.git"
+	echo "						or <user>@gitlab-polyglot.hursley.ibm.com:<namespace>/tr.open.git "
+	echo "	-jit-branch			the OpenJ9/jit git branch: java-master "
 	echo " "
 	exit 1
 }
@@ -33,8 +41,8 @@ fi
 
 declare -A j9repos
 declare -A branches
-declare -A default_j9repos=( [j9vm]=j9/j9vm [j9jcl]=joe_dekoning-ca/j9jcl [omr]=omr/omr [binaries]=j9/binaries [tooling]=j9/tooling [rtctest]=j9/rtctest [test]=j9/test [tr.open]=jit/tr.open )
-declare -A default_branches=( [j9vm]=master [j9jcl]=master [omr]=java-master [binaries]=master [tooling]=master [rtctest]=master [test]=master [tr.open]=java-master )
+declare -A default_j9repos=( [j9vm]=j9/j9vm [j9jcl]=joe_dekoning-ca/j9jcl [omr]=omr/omr [binaries]=j9/binaries [tooling]=j9/tooling [rtctest]=j9/rtctest [j9test]=j9/test [tr.open]=jit/tr.open )
+declare -A default_branches=( [j9vm]=master [j9jcl]=master [omr]=java-master [binaries]=master [tooling]=master [rtctest]=master [j9test]=master [tr.open]=java-master )
 
 
 ostype=`uname -s`
@@ -138,36 +146,31 @@ protocol=https
 if [[ $repo_url == *"$protocol"* ]] ; then 
 	# http protocol: e.g. https://gitlab-polyglot.hursley.ibm.com/omr/openjdk.git
 	b=`expr index ${repo_url:8} /`
-	git_url=`expr substr ${repo_url:8} 1 $b`
-	git_url="https://${git_url}"
+	base_git_url=`expr substr ${repo_url:8} 1 $b`
+	base_git_url="https://${base_git_url}"
 else
 	# ssh protocol: e.g. git002@gitlab-polyglot.hursley.ibm.com:omr/openjdk.git
 	b=`expr index $repo_url :`
-	git_url=`expr substr $repo_url 1 $b`
+	base_git_url=`expr substr $repo_url 1 $b`
 fi
 
 # clone OpenJ9 repos
 echo "Get OpenJ9 sources"
 
 for i in "${!default_j9repos[@]}" ; do
-	# work-around for test repo
-	if [ ${i} = "test"  ]; then
-		output="j9test"
-	else
-		output=$i
-	fi 
 	# clone repo
 	branch=${default_branches[$i]}
 	if [ ${branches[$i]+_} ]; then
 		branch=${branches[$i]}
-		fi
-
-		repo="${default_j9repos[$i]}.git"
+	fi
+	
+	git_url=${base_git_url}${default_j9repos[$i]}.git
+	
 	if [ ${j9repos[$i]+_} ]; then
-		repo="${j9repos[$i]}.git"
-		fi
+		git_url="${j9repos[$i]}"
+	fi
 
-		git_clone_command="${git} clone --recursive -b ${branch} ${git_url}${repo} ${output}"
+	git_clone_command="${git} clone --recursive -b ${branch} ${git_url} ${i}"
 	echo "Servicing $i"
 	${git_clone_command} || exit $?
 done
