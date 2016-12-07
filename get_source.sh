1b2edab1f3892e5c7facdae5ca85d999ef826c9f
openj9 <j9build@ca.ibm.com>
2016-12-07 11:03:08 -0500

    Update get_sources.sh and patches directory for b147 support
    Signed-off-by: openj9 <j9build@ca.ibm.com>

--- get_source.sh
+++ get_source.sh
@@ -52,7 +52,7 @@ usage() {
 	echo "Usage: $0 [-h|--help] [-r|--revision=<tag>] [-j9|--with-j9] [... other j9 options]"
 	echo "where:"
 	echo "	-h|--help 				print this help, then exit"
-	echo "	-r|--revision=<tag> 	is one of: jdk-9+95, jdk-9+110, jdk-9+111, jdk-9+113 jdk-9+136 jdk-9+139 jdk-9+141 jdk-9+143 jdk-9+145"
+	echo "	-r|--revision=<tag> 	is one of: jdk-9+145 jdk-9+147"
 	echo "							[Note: fetch the given revision, otherwise get the latest sources"
 	echo "	-j9|--with-j9 			get the OpenJ9 latest sources "
 	echo " other j9 options (used only with -j9|--with-j9 option): "
@@ -76,7 +76,7 @@ usage() {
 }
 
 j9flag="false"
-hgtag="jdk-9+145"
+hgtag="jdk-9+147"
 
 
 for i in "$@"
@@ -113,7 +113,7 @@ do
 done
 
 # expected OpenJDK tags
-hgtags="jdk-9+95 jdk-9+110 jdk-9+111 jdk-9+113 jdk-9+136 jdk-9+139 jdk-9+141 jdk-9+143 jdk-9+145"
+hgtags="jdk-9+145 jdk-9+147"
 
 # check if sources loaded
 has_sources="false"
