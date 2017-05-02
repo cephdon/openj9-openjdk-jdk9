2f0f74e8750ee0f2dbe43122162775af5d0fe2c6
Keith Campbell <keithc@ca.ibm.com>
2017-05-02 18:26:29 -0400

    Streamline stage-j9 target (#147)
    * use marker files to identify files which have been changed since
      the last build
    * set SHA in j9version.h reliably
    * get_source.sh: only check for Mercurial if we need it
    
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- get_source.sh
+++ get_source.sh
@@ -26,26 +26,26 @@
 #
 
 to_stderr() {
-    echo "$@" >&2
+	echo "$@" >&2
 }
 
 error() {
-    to_stderr "ERROR: $1"
-    exit ${2:-126}
+	to_stderr "ERROR: $1"
+	exit ${2:-126}
 }
 
 warning() {
-    to_stderr "WARNING: $1"
+	to_stderr "WARNING: $1"
 }
 
 version_field() {
-  # rev is typically omitted for minor and major releases
-  field=`echo ${1}.0 | cut -f ${2} -d .`
-  if expr 1 + $field >/dev/null 2> /dev/null; then
-    echo $field
-  else
-    echo -1
-  fi
+	# rev is typically omitted for minor and major releases
+	field=`echo ${1}.0 | cut -f ${2} -d .`
+	if expr 1 + $field >/dev/null 2>&1 ; then
+		echo $field
+	else
+		echo -1
+	fi
 }
 
 usage() {
@@ -77,54 +77,53 @@ usage() {
 	exit 1
 }
 
-j9flag="false"
+j9flag=false
 tag="jdk-9+162"
 
-for i in "$@"
-do
+for i in "$@" ; do
 	case $i in
 		-h | --help )
-		usage
-		;;
+			usage
+			;;
 
 		-j9 | --with-j9 )
-		j9flag="true"
-		;;
+			j9flag=true
+			;;
 
 		-j9vm-repo=* | -j9vm-branch=* | -omr-repo=* | -omr-branch=* | -binaries-repo=* | -binaries-branch=* | -jit-repo=* |-jit-branch=* )
-		j9options="${j9options} ${i}"
-		;;
+			j9options="${j9options} ${i}"
+			;;
 
 		-j9vmSHA=* | -omrSHA=* | -binariesSHA=* | -jitSHA=* )
-		j9options="${j9options} ${i}"
-		;;
+			j9options="${j9options} ${i}"
+			;;
 
 		-parallel=* )
-		j9options="${j9options} ${i}"
-		;;
+			j9options="${j9options} ${i}"
+			;;
 
 		-r=* | --revision=* )
-		tag="${i#*=}"
-		;;
+			tag="${i#*=}"
+			;;
 
 		'--' ) # no more options
-		usage
-		;;
+			usage
+			;;
 
-		-*)  # bad option
-		usage
-		;;
+		-*) # bad option
+			usage
+			;;
 
-		*)  # bad option
-		usage
-		;;
+		*) # bad option
+			usage
+			;;
 	esac
 done
 
 # expected OpenJDK tags
 hgtags="jdk-9+162"
 
-if [ -n  "$tag" ]; then
+if [ -n "$tag" ]; then
 	good_tag="false"
 
 	for hgtag in ${hgtags} ; do
@@ -141,53 +140,54 @@ fi
 
 # Version check
 
-# required
-reqdmajor=1
-reqdminor=4
-reqdrev=0
+hg_version_check() {
+	# required
+	reqdmajor=1
+	reqdminor=4
+	reqdrev=0
 
-# requested
-rqstmajor=2
-rqstminor=6
-rqstrev=3
+	# requested
+	rqstmajor=2
+	rqstminor=6
+	rqstrev=3
 
+	# installed
+	hgwhere="`command -v hg`"
+	if [ "x$hgwhere" = "x" ]; then
+		error "Could not locate Mercurial command"
+	fi
 
-# installed
-hgwhere="`command -v hg`"
-if [ "x$hgwhere" = "x" ]; then
-  error "Could not locate Mercurial command"
-fi
+	hgversion="`LANGUAGE=en hg --version 2> /dev/null | sed -n -e 's@^Mercurial Distributed SCM (version \([^+]*\).*)\$@\1@p'`"
+	if [ "x${hgversion}" = "x" ] ; then
+		error "Could not determine Mercurial version of $hgwhere"
+	fi
 
-hgversion="`LANGUAGE=en hg --version 2> /dev/null | sed -n -e 's@^Mercurial Distributed SCM (version \([^+]*\).*)\$@\1@p'`"
-if [ "x${hgversion}" = "x" ] ; then
-  error "Could not determine Mercurial version of $hgwhere"
-fi
+	hgmajor="`version_field $hgversion 1`"
+	hgminor="`version_field $hgversion 2`"
+	hgrev="`version_field $hgversion 3`"
 
-hgmajor="`version_field $hgversion 1`"
-hgminor="`version_field $hgversion 2`"
-hgrev="`version_field $hgversion 3`"
+	if [ $hgmajor -eq -1 -o $hgminor -eq -1 -o $hgrev -eq -1 ] ; then
+		error "Could not determine Mercurial version of $hgwhere from \"$hgversion\""
+	fi
 
-if [ $hgmajor -eq -1 -o $hgminor -eq -1 -o $hgrev -eq -1 ] ; then
-  error "Could not determine Mercurial version of $hgwhere from \"$hgversion\""
-fi
+	# Require
+	if [ $hgmajor -lt $reqdmajor -o \( $hgmajor -eq $reqdmajor -a $hgminor -lt $reqdminor \) -o \( $hgmajor -eq $reqdmajor -a $hgminor -eq $reqdminor -a $hgrev -lt $reqdrev \) ] ; then
+		error "Mercurial version $reqdmajor.$reqdminor.$reqdrev or later is required. $hgwhere is version $hgversion"
+	fi
 
+	# Request
+	if [ $hgmajor -lt $rqstmajor -o \( $hgmajor -eq $rqstmajor -a $hgminor -lt $rqstminor \) -o \( $hgmajor -eq $rqstmajor -a $hgminor -eq $rqstminor -a $hgrev -lt $rqstrev \) ] ; then
+		warning "Mercurial version $rqstmajor.$rqstminor.$rqstrev or later is recommended. $hgwhere is version $hgversion"
+	fi
+}
 
-# Require
-if [ $hgmajor -lt $reqdmajor -o \( $hgmajor -eq $reqdmajor -a $hgminor -lt $reqdminor \) -o \( $hgmajor -eq $reqdmajor -a $hgminor -eq $reqdminor -a $hgrev -lt $reqdrev \) ] ; then
-  error "Mercurial version $reqdmajor.$reqdminor.$reqdrev or later is required. $hgwhere is version $hgversion"
-fi
-
-
-# Request
-if [ $hgmajor -lt $rqstmajor -o \( $hgmajor -eq $rqstmajor -a $hgminor -lt $rqstminor \) -o \( $hgmajor -eq $rqstmajor -a $hgminor -eq $rqstminor -a $hgrev -lt $rqstrev \) ] ; then
-  warning "Mercurial version $rqstmajor.$rqstminor.$rqstrev or later is recommended. $hgwhere is version $hgversion"
-fi
-
-if [ ${j9flag} = "true" ] ; then
+if [ "${j9flag}" = true ] ; then
 	# Get clones of OpenJ9 absent repositories
-	bash ./openj9/get_j9_source.sh ${j9options}
+	bash openj9/get_j9_source.sh ${j9options}
 else
-	if [ -d hotspot]; then
+	hg_version_check
+
+	if [ -d hotspot ] ; then
 		# update hotspot
 		echo
 		echo "Update hotspot source"
@@ -207,4 +207,3 @@ else
 
 	git checkout tags/${tag} || exit $?
 fi
-
